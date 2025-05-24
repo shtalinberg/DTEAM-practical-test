59eaaecc394feb8eb2939b17ebbcff1d119078b3
Oleksandr Shtalinberg <o.shtalinberg@gmail.com>
2025-05-24 14:40:13 +0000

    add production configs

--- sc_backend/djproject/settings/base_django.py
+++ sc_backend/djproject/settings/base_django.py
@@ -39,7 +39,7 @@ SECRET_KEY = os.environ.get(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", [])
 
 
 # Application definition
---
+++ sc_backend/djproject/settings/production.py
@@ -0,0 +1,17 @@
+import os
+
+from .base_django import *
+
+DEBUG = False
+
+# Database
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DB', ''),
+        'USER': os.getenv('POSTGRES_USER', ''),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
+        'HOST': os.getenv('POSTGRES_HOST', 'localhost'),
+        'PORT': os.getenv('POSTGRES_PORT', '5432'),
+    }
+}
