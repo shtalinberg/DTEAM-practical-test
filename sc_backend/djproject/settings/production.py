6399481d055fae343aed2b7e228454b30388de2e
Oleksandr Shtalinberg <o.shtalinberg@gmail.com>
2025-05-24 14:46:51 +0000

    add poetry dj-database-url

--- sc_backend/djproject/settings/production.py
+++ sc_backend/djproject/settings/production.py
@@ -1,17 +1,10 @@
 import os
 
+import dj_database_url
+
 from .base_django import *
 
 DEBUG = False
 
-# Database
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('POSTGRES_DB', ''),
-        'USER': os.getenv('POSTGRES_USER', ''),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
-        'HOST': os.getenv('POSTGRES_HOST', 'localhost'),
-        'PORT': os.getenv('POSTGRES_PORT', '5432'),
-    }
-}
+
+DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))}
